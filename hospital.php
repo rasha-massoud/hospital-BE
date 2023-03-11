d630ef47c47b3ffafd90013f2d7fbc30e6716b19
RashaM <rashamassoud1998@gmail.com>
2023-03-12 01:26:51 +0200

    Created the hospitals' BE

---
+++ hospital.php
@@ -0,0 +1,24 @@
+<?php
+    include("connection.php"); 
+    session_start();
+    // $_SESSION['id'] = $id;
+
+    $hospitals = [];
+    $query = $mysqli->prepare("SELECT * FROM hospitals");
+    $query->execute();
+    $query->bind_result($id, $name, $address, $phone_number, $email, $facebook_url);
+
+    while ($query->fetch()) {
+        $hospital = [
+            "id" => $id,
+            "name" => $name,
+            "address" => $address,
+            "phone_number" => $phone_number,
+            "email" => $email,
+            "facebook_url" => $facebook_url,
+        ];
+        $hospitals[] = $hospital;
+    }
+
+    echo json_encode($hospitals);
+?>
