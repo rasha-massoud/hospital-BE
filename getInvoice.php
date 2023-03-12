a447e97155aa47e2ae6d2931ae8c6fded5765a92
RashaM <rashamassoud1998@gmail.com>
2023-03-13 01:42:56 +0200

    Modified them invoice API

--- getInvoice.php
+++ getInvoice.php
@@ -9,20 +9,28 @@
     $check_patient_id->store_result();
     $patient_id_exists = $check_patient_id->num_rows();
 
-    if ($patient_id_exists > 0) {
-        $query = $mysqli->prepare("select id, user_id, blood_type, EHR from patients_info where user_id=?");
-        $query->bind_param('i', $user_id);
-        $query->execute();
-        $query->bind_result($id, $user_id, $blood_type, $EHR);
 
-        if ($query->fetch()){
-            $response = [
-                "id" => $id,
-                "user_id" => $user_id,
-                "blood_type" => $blood_type,
-                "EHR" => $EHR,
+    if ($patient_id_exists > 0) {
+
+    $approved=1;
+        $servicequery = $mysqli->prepare("select id, employee_id, patient_id, description, cost, department_id, approved from services where patient_id=? and approved=?");
+        $servicequery->bind_param('ii', $user_id, $approved);
+        $servicequery->execute();
+        $servicequery->bind_result($id, $employee_id, $patient_id, $description, $cost, $department_id, $approved);
+
+        while ($servicequery->fetch()) {
+            $services[] = [
+                'id' => $id,
+                'employee_id' => $employee_id,
+                'patient_id' => $patient_id,
+                'description' => $description,
+                'cost' => $cost,
+                'department_id' => $department_id,
+                'approved' => $approved,
             ];
         }
+
+        $response['services'] = $services;
     }
     else{
         $response['status']= "Not found patient id";
