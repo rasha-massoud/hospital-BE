3fdccc30afdbf639fcaa1d55e3ee88c8a00461a8
RashaM <rashamassoud1998@gmail.com>
2023-03-12 03:41:28 +0200

    Added the link page display between patients and hospitals

---
+++ patientToHospDisplay.php
@@ -0,0 +1,23 @@
+<?php
+    include("connection.php"); 
+    session_start();
+    // $_SESSION['id'] = $id;
+
+    $links = [];
+    $query = $mysqli->prepare("SELECT * FROM hospital_users");
+    $query->execute();
+    $query->bind_result($hospital_id, $user_id, $is_active, $date_joined, $date_left);
+
+    while ($query->fetch()) {
+        $link = [
+            "hospital_id" => $hospital_id,
+            "user_id" => $user_id,
+            "is_active" => $is_active,
+            "date_joined" => $date_joined,
+            "posidate_lefttion" => $date_left,
+        ];
+        $links[] = $link;
+    }
+
+    echo json_encode($links);
+?>
