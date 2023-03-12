c3b00cbe3140ad26ee43f2974682c0c9c8a28e8d
RashaM <rashamassoud1998@gmail.com>
2023-03-12 14:00:01 +0200

    Added the request API

---
+++ acceptingRequests.php
@@ -0,0 +1,37 @@
+<?php
+    include("connection.php"); 
+    session_start();
+
+    $employer_id= $_POST['employer_id'];
+    $patient_id = $_POST['patient_id'];
+    $description = $_POST['description'];
+    $cost = $_POST['cost'];
+    $department_id = $_POST['department_id'];
+    $approved = $_POST['approved'];
+
+    $response = [];
+
+    $check_patient_id = $mysqli->prepare('select user_id from patients_info where user_id=?');
+    $check_patient_id->bind_param('i', $patient_id);
+    $check_patient_id->execute();
+    $check_patient_id->store_result();
+    $user_id_exists = $check_patient_id->num_rows();
+
+    $check_employer_id = $mysqli->prepare('select user_id from employees_info where user_id=?');
+    $check_employer_id->bind_param('i', $employer_id);
+    $check_employer_id->execute();
+    $check_employer_id->store_result();
+    $employer_id_exists = $check_employer_id->num_rows();
+
+    if ($user_id_exists > 0 && $employer_id_exists > 0) {
+        $query = $mysqli->prepare('update services set approved=? where employee_id=? and patient_id=? and description=? and cost=? and department_id=?');
+        $query->bind_param('iiisii', $approved, $employee_id, $patient_id, $description, $cost, $department_id);
+        $query->execute();
+        $response['status'] = "success";
+    } else {
+        $response['status'] = "failed";
+    }
+    
+    echo json_encode($response);
+
+?>
