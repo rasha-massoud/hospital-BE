ee5e90d8969509384d7e4f17e86d6e4a1e5c16b6
RashaM <rashamassoud1998@gmail.com>
2023-03-12 07:32:43 +0200

    Added the required API for the Service feature

---
+++ awaitingRequests.php
@@ -0,0 +1,25 @@
+<?php
+    include("connection.php"); 
+    session_start();
+    // $_SESSION['id'] = $id;
+
+    $requests = [];
+    $query = $mysqli->prepare("SELECT * FROM services");
+    $query->execute();
+    $query->bind_result($id, $employee_id  , $patient_id , $description, $cost, $department_id, $approved);
+
+    while ($query->fetch()) {
+        $request = [
+            "id"=>$id ,
+            "employee_id " => $employee_id ,
+            "patient_id" => $patient_id,
+            "description" => $description,
+            "cost" => $cost,
+            "department_id" => $department_id,
+            "approved" => $approved,
+        ];
+        $requests[] = $request;
+    }
+
+    echo json_encode($requests);
+?>
