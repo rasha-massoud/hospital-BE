ca4e62dbdb85cd2f7442eeb745300ba1a3b645b0
RashaM <rashamassoud1998@gmail.com>
2023-03-13 05:11:40 +0200

    Added the statistics API

---
+++ statistics.php
@@ -0,0 +1,24 @@
+<?php
+    include("connection.php"); 
+
+    session_start();
+    $user_type_id = $_POST['user_type_id'];
+    
+    if ($user_type_id == 1) { // user is a patient
+        $query = $mysqli->prepare('SELECT COUNT(*) FROM users WHERE user_type_id = ?');
+        $query->bind_param('i', $user_type_id);
+        $query->execute();
+        $query->bind_result($num_patients);
+        $query->fetch();
+    
+        $response['status']= "There are " . $num_patients . " patients.";
+    } else if ($user_type_id == 2) { // user is an employee
+        $query = $mysqli->prepare('SELECT COUNT(*) FROM users WHERE user_type_id = 2');
+        $query->execute();
+        $query->bind_result($num_employees);
+        $query->fetch();
+    
+        $response['status']= "There are " . $num_employees . " employees.";
+    }
+    echo json_encode($response);
+?>
