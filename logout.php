bfb2bb92109ddbcaac24286f60ec8aa8923ad9da
RashaM <rashamassoud1998@gmail.com>
2023-03-13 07:00:13 +0200

    Added the logout API

--- logout.php
+++ logout.php
@@ -2,5 +2,5 @@
     session_start();
     unset($_SESSION['jwt_token']);
     session_destroy();
-    header("Location: index.php"); // or wherever you want to redirect after logout
+    header("Location: login.php"); // or wherever you want to redirect after logout
 ?>
